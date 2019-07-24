2dfa99b4d6f8e725b10e663fb97538e8ac625f88
宮里航大 <miyazatokodai@miyasatokoudainoMacBook-puro.local>
2019-07-24 17:08:34 +0900

    feat' initial commit

---
+++ tmp/constants.py
@@ -0,0 +1,11 @@
+""" Constants file for Auth0's seed project
+"""
+AUTH0_CLIENT_ID = 'bauTfD1fmT276fkYR5qCK4MHPDilIKkv'
+AUTH0_CLIENT_SECRET = 'URALjh7LrTd4AIdwqoBWg1M-czadWTR-4F9BfkcV8_gsaoq3VO5TKZFSbQIFnSKq'
+AUTH0_CALLBACK_URL = 'https://localhost:8000/auth0/callback'
+AUTH0_DOMAIN = 'practice-auth.auth0.com'
+AUTH0_AUDIENCE = 'AUTH0_AUDIENCE'
+PROFILE_KEY = 'profile'
+SECRET_KEY = 'ThisIsTheSecretKey'
+JWT_PAYLOAD = 'jwt_payload'
+
